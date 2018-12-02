2698e19ef1e309e1846f1b3aa5a83ab475dd69b8
knightingal <yujianming1985@hotmail.com>
2018-12-02 19:49:54 +0800

    divide front end project

--- js/my-addon-irt/data/dialog.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn_ok").onclick = function() {
-    console.log("btn ok clicked");
-    self.port.emit("dialog_resp", true);
-};
-
-document.getElementById("btn_cancle").onclick = function() {
-    console.log("btn cancle clicked");
-    self.port.emit("dialog_resp", false);
-};
--- nodejs/node_server/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-    password:"******",
-    dbpassword:"******"
-};
-
-module.exports = config;
