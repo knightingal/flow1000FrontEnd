044966ef11a3236014ef126a8d3f612b3b92c087
knightingal <yujianming1985@hotmail.com>
2019-01-06 16:47:49 +0800

    lazyload section list

--- src/components/SectionList.tsx
+++ src/components/SectionList.tsx
@@ -8,26 +8,44 @@ class SectionBean {
 
 export class SectionList extends React.Component<{container: Container}, {sectionList:Array<SectionBean>, selectedIndex: string}> {
     url:URL;
+    battleShipPage:Boolean;
+    sectionItemHeightStep:Array<Number>;
+    divRefs:React.RefObject<HTMLDivElement>;
+
     constructor(props:{container: Container}) {
         super(props);
         this.state = {sectionList:[], selectedIndex: null};
         this.url = new URL(document.URL);
+        this.battleShipPage = this.url.pathname.indexOf("battleships.html") >= 0;
+        this.divRefs = React.createRef();
     }
 
     fecthSectionList() {
         let fetchUrl:string;
-        if (this.url.pathname.indexOf("battleships.html") >= 0) {
-            fetchUrl = "/local1000/picIndexAjax?album=BattleShips";
-        } else {
-            fetchUrl = "/local1000/picIndexAjax";
-        }
+        fetchUrl = this.battleShipPage ? "/local1000/picIndexAjax?album=BattleShips" : "/local1000/picIndexAjax";
+
         console.log("fetchUrl is " + fetchUrl);
         fetch(fetchUrl)
         .then((resp: Response) => {
             return resp.json();
         })
         .then((json: any) => {
-            const sectionList:Array<SectionBean> = json;
+            let sectionList:Array<SectionBean> = json;
+            if (this.battleShipPage == true) {
+                //sectionList.(sectionList);
+                sectionList = [].concat(
+                    sectionList, sectionList, sectionList, sectionList,
+                    sectionList, sectionList, sectionList, sectionList,
+                    sectionList, sectionList, sectionList, sectionList,
+                    sectionList, sectionList, sectionList, sectionList
+                );
+            }
+            this.sectionItemHeightStep = sectionList.map((value, index, array) => {
+                return 19 * index;
+            });
+            this.currentTopPicIndex = 0;
+            this.currentButtonPicIndex = this.checkPostionInPic(this.divRefs.current.clientHeight);
+
             this.setState({
                 sectionList: sectionList
             });
@@ -39,6 +57,40 @@ export class SectionList extends React.Component<{container: Container}, {sectio
         this.setState({
             selectedIndex: index
         });
+    }
+
+    currentTopPicIndex: number;
+    currentButtonPicIndex: number;
+    
+    scrollHandler(e : React.UIEvent) {
+        // this is a lazyload implament
+        const scrollTop: number = (e.target as HTMLDivElement).scrollTop;
+        const clientHeight: number = (e.target as HTMLDivElement).clientHeight;
+        let update = false;
+        // calculate the index of top picture after scroll
+        const refreshTopPicIndex = this.checkPostionInPic(scrollTop);
+        if (refreshTopPicIndex !== this.currentTopPicIndex) {
+            this.currentTopPicIndex = refreshTopPicIndex;
+            console.log(`change top to pic index: ${this.currentTopPicIndex}`);
+            update = true;
+        }
+        // calculate the index of button picture after scroll
+        const refreshButtonPicIndex = this.checkPostionInPic(scrollTop + clientHeight);
+        if (refreshButtonPicIndex !== this.currentButtonPicIndex) {
+            this.currentButtonPicIndex = refreshButtonPicIndex;
+            console.log(`change button to pic index: ${this.currentButtonPicIndex}`);
+            update = true;
+        }
+        // if any index of picture changed, re-render the page
+        if (update) {
+            this.setState(this.state);
+        }
+    }
+
+    checkPostionInPic(postion: number): number {
+        return this.sectionItemHeightStep.filter((height) => {
+            return height < postion;
+        }).length - 1;
     }
 
     componentDidMount() {
@@ -55,18 +107,20 @@ export class SectionList extends React.Component<{container: Container}, {sectio
     }
 
     render() {
-        return <div className="SectionList">
+        return <div className="SectionList" onScroll={(e) => this.scrollHandler(e)} ref={this.divRefs}>
             {this.state.sectionList.map((sectionBean: SectionBean, index: number) => {
-                return (<div 
+                const displayImg = index >= this.currentTopPicIndex - 1 && index <= this.currentButtonPicIndex + 1; 
+                return displayImg ?
+                (<div 
                     key={index} 
                     onClick={(e) => this.handleSectionClick(e, sectionBean.index)} 
-                    // style={{backgroundColor: (sectionBean.index === this.state.selectedIndex ? 'yellow' : 'white')}}
                 >
                     <a 
                         className="SectionListItem" 
                         style={{fontFamily:"DejaVu Sans", color:(sectionBean.index === this.state.selectedIndex ? '#35b5ff' : 'black')}}
                     >{sectionBean.name}</a>
-                </div>);
+                </div>) 
+                : (<div key={index} style={{height:"19px"}}></div>);
             })}
         </div>
     }
